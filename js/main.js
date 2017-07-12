a17d08e6065e2f21ebd327d75322a94a0545f54c
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-07-12 16:44:34 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,42 +1,9 @@
 // console.log("Up and running!");
 
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
 
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardThree);
-
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-// var cardOne = cards[0];
-
-function checkForMatch() {
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-    } else {
-      alert("Sorry, try again.");
-    }
-  }
-}
-
-function flipCard(cardId) {
-  // cardsInPlay.push(cardOne);
-  // console.log(`User flipped ${cardOne}`);
-
-  // var cardTwo = cards[2];
-  // cardsInPlay.push(cardTwo);
-  // console.log(`User flipped ${cardTwo}`);
-
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-
-  checkForMatch();
-}
-
-flipCard(0);
-flipCard(2);
-
-
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
